62604f70b8b239368c900ce243fc0f730091e0f2
Ivgeniay <motleycrue6502@gmail.com>
2022-11-22 16:36:14 +0400

    22.11.22 16.40

--- Assets/Scripts/Enemies/DamageMech/IDestroyable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-
-namespace Assets.Scripts.Enemies.DamageMech
-{
-    public interface IDestroyable
-    {
-        void Destroy();
-    }
-}
--- Assets/Scripts/Enemies/StateMech/States/AnimationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class AnimationConstants
-{
-    public const string IsWalking = "IsWalking";
-    public const string WalkType = "WalkType";
-    public const string Attack = "Attack";
-    public const string AttackType = "AttackType";
-}
-
